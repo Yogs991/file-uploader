d1ffe3643d25ac5f9132dbc8739c31dbab393ed1
Yog99 <124916155+Yogs991@users.noreply.github.com>
2026-02-21 14:23:12 +0200

    added upload file functionality

--- controller/controller.js
+++ controller/controller.js
@@ -2,10 +2,28 @@ require("dotenv").config();
 const prisma = require('../lib/prisma.js');
 const bcrypt = require("bcryptjs");
 const multer = require("multer");
-const upload = multer({dest: 'uploads/'});
+const supabase = require("../lib/supabase.js");
+// const upload = multer({dest: 'uploads/'});
+
+const storage = multer.memoryStorage();
+const uploadMulter = multer({
+    storage: storage, limits: {fileSize: 1_000_000}
+});
 
 async function getMainPage(req, res){
-    res.render("index",{user: req.user});
+    try {
+        if(!req.user){
+            return res.render("index", {user:null, folders:[]});
+        }
+        const folders = await prisma.folder.findMany({
+        where: {
+            ownerId: req.user.id,
+        }
+    });
+    res.render("index",{user: req.user, folders: folders});
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 async function getLoginPage(req, res){
@@ -47,10 +65,94 @@ async function getUserFromDb(req,res){
     }
 }
 
+async function getCreateFolderForm(req,res){
+    res.render("create-folder");
+}
+
+async function createFolder(req, res){
+    try {
+        const newFolder = await prisma.folder.create({
+            data:{
+                name: req.body.folderName,
+                ownerId: req.user.id
+            }
+        })
+        console.log(`${newFolder} successfully created`);
+        res.redirect("/");
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function uploadFile(req,res){
+    try {
+        if(!req.user){
+            res.redirect("/log-in");
+        }
+
+        if(!req.file){
+            return res.status(400).send("No file uploaded");
+        }
+
+        const {folderId} = req.body;
+
+        if(!folderId || folderId === "None"){
+            return res.status(400).send("Please select a folder");
+        }
+
+        const folder = await prisma.folder.findFirst({
+            where:{
+                id: parseInt(folderId),
+                ownerId: req.user.id
+            }
+        });
+
+        if(!folder){
+            return res.status(400).send("Folder not found");
+        }
+
+        const filePath = `${req.user.id}/${folder.id}/${req.file.originalname}_${Date.now()}`;
+
+        const {data, error} =  await supabase.storage.from("uploads").upload(filePath, req.file.buffer,{
+            contentType: req.file.mimetype,
+            upsert: false,
+        });
+
+        
+        
+        if(error){
+            throw error;
+        }else{
+            console.log("uploaded file to supabase", data);
+        }
+
+        const {data: urlData} = supabase.storage.from("uploads").getPublicUrl(filePath);
+        
+        await prisma.file.create({
+            data:{
+                title: req.file.originalname,
+                link: urlData.publicUrl,
+                size: req.file.size,
+                uploaderId: req.user.id,
+                folderId: folder.id
+            }
+        });
+
+        res.redirect("/");
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
 module.exports = {
+    uploadMulter,
     getMainPage,
     getLoginPage,
     getSignUpPage,
     saveUser,
-    getUserFromDb
+    getUserFromDb,
+    getCreateFolderForm,
+    createFolder,
+    uploadFile
 }
--- route/router.js
+++ route/router.js
@@ -5,16 +5,22 @@ const controller = require("../controller/controller");
 const passport = require("passport");
 
 router.get("/", controller.getMainPage);
+
 router.get("/log-in", controller.getLoginPage);
+
 router.get("/sign-up", controller.getSignUpPage);
+
+router.get("/create-folder",controller.getCreateFolderForm)
 
 router.post("/log-in",passport.authenticate("local",
     {
         successRedirect: "/",
         failureRedirect: "/log-in"
     }
-))
+));
+
 router.post("/sign-up", controller.saveUser);
+
 router.post("/log-out", (req, res, next)=>{
     req.logout((err)=>{
         if(err){
@@ -22,7 +28,11 @@ router.post("/log-out", (req, res, next)=>{
         }
         res.redirect("/");
     })
-})
+});
+
+router.post("/create-folder", controller.createFolder);
+
+router.post("/upload", controller.uploadMulter.single('file') ,controller.uploadFile);
 
 
 module.exports = router;
